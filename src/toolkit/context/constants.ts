d759f7e14382ba8c9c536525d9b3697cb43fd1a6
lliepert-wish <lliepert@wish.com>
2021-09-09 14:22:58 -0400

    [MKL-43855] Fix URLs (#17)

--- src/toolkit/context/constants.ts
+++ src/toolkit/context/constants.ts
@@ -2,16 +2,16 @@ export const MD_URL =
   process.env.ENV === "prod"
     ? "https://www.merchant.wish.com"
     : process.env.ENV === "stage"
-    ? "https://www.staging.merchant.wish.com"
+    ? "https://staging.merchant.wish.com"
     : process.env.ENV === "dev"
-    ? "https://www.staging.merchant.wish.com"
+    ? "https://staging.merchant.wish.com"
     : process.env.NEXT_PUBLIC_MD_URL || "";
 
 export const WISH_URL =
   process.env.ENV === "prod"
     ? "https://www.wish.com"
     : process.env.ENV === "stage"
-    ? "https://www.staging.wish.com"
+    ? "https://staging.wish.com"
     : process.env.ENV === "dev"
-    ? "https://www.staging.wish.com"
+    ? "https://staging.wish.com"
     : process.env.NEXT_PUBLIC_WISH_URL || "";
