6da94ea8f36948ae56a0a86fce3d656233e3a282
Lucas Liepert <lliepert@wish.com>
2023-01-24 19:00:06 -0500

    [MKL-70161] TA and GTM integration (#141)

--- src/pages/_app.prod.tsx
+++ src/pages/_app.prod.tsx
@@ -2,7 +2,11 @@ import "../styles/global.css";
 
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import MerchantDashboardProvider from "@chrome/MerchantDashboardProvider";
+import { isProd } from "@core/stores/EnvironmentStore";
+
+const TRUSTARC_DOMAIN = isProd ? "wish.com" : "wish-test.com";
 
 const MerchantDashboard = ({
   Component,
@@ -36,6 +40,92 @@ const App = (props: AppProps): JSX.Element => {
       <Head>
         <title>Wish for Merchants</title>
       </Head>
+      <Script
+        id="ta-setup"
+        src={`//consent.trustarc.com/notice?domain=${TRUSTARC_DOMAIN}&c=teconsent&text=true&js=nj&gtm=1&noticeType=bb`}
+      />
+      <Script id="gtm-setup">
+        {`
+          (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+            var f = d.getElementsByTagName(s)[0],
+              j = d.createElement(s),
+              dl = l != "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+          })(window, document, "script", "dataLayer", "GTM-N7HNVP4");
+        `}
+      </Script>
+      <Script id="ta-gtm-integration">
+        {`
+          var __dispatched__ = {}; //Map of previously dispatched preference levels
+          /*
+          First step is to register with the CM API to receive callbacks when a preference update
+          occurs. You must wait for the CM API (PrivacyManagerAPI object) to exist on the page before
+          registering.
+          */
+          var __i__ =
+            self.postMessage &&
+            setInterval(function () {
+              if (self.PrivacyManagerAPI && __i__) {
+                var apiObject = {
+                  PrivacyManagerAPI: {
+                    action: "getConsentDecision",
+                    timestamp: new Date().getTime(),
+                    self: self.location.host,
+                  },
+                };
+                self.top.postMessage(JSON.stringify(apiObject), "*");
+                __i__ = clearInterval(__i__);
+              }
+            }, 50);
+          /*
+          Callbacks will occur in the form of a PostMessage event. This code listens for the
+          appropriately formatted PostMessage event, gets the new consent decision, and then pushes
+          the events into the GTM framework. Once the event is submitted, that consent decision is
+          marked in the 'dispatched' map so it does not occur more than once.
+          */
+          self.addEventListener("message", function (e, d) {
+            try {
+              if (
+                e.data &&
+                (d = JSON.parse(e.data)) &&
+                (d = d.PrivacyManagerAPI) &&
+                d.capabilities &&
+                d.action == "getConsentDecision"
+              ) {
+                var newDecision = self.PrivacyManagerAPI.callApi(
+                  "getGDPRConsentDecision",
+                  self.location.host
+                ).consentDecision;
+                newDecision &&
+                  newDecision.forEach(function (label) {
+                    if (!__dispatched__[label]) {
+                      self.dataLayer &&
+                        self.dataLayer.push({ event: "GDPR Pref Allows " + label });
+                      __dispatched__[label] = 1;
+                    }
+                  });
+              }
+            } catch (xx) {
+              /** not a cm api message **/
+            }
+          });
+        `}
+      </Script>
+      {/* GTM and TA required body html */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-N7HNVP4"
+          height="0"
+          width="0"
+          className="gtm-noscript"
+        ></iframe>
+      </noscript>
+      <div id="consent_blackbar"></div>
+      <div id="teconsent"></div>
       {typeof window === "undefined" ? null : <MerchantDashboard {...props} />}
     </div>
   );
