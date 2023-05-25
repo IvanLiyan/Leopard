c95b6f7c5566fc221d805376bbcc5e1202453305
Lucas Liepert <lliepert@wish.com>
2023-05-25 11:32:32 -0400

    add disabled prop to radio card (#251)

--- src/app/core/components/RadioCard.tsx
+++ src/app/core/components/RadioCard.tsx
@@ -8,34 +8,38 @@ export type RadioButtonCardProps = Omit<CardProps, "onClick"> & {
   readonly checked: RadioProps["checked"];
   readonly onChange: CardProps["onChange"];
   readonly text?: TextProps["children"];
+  readonly disabled?: boolean;
 };
 
 const RadioCard: React.FC<RadioButtonCardProps> = ({
   checked,
   onChange: onCheck,
   text,
+  disabled,
   children,
   sx,
   ...props
 }) => {
-  const { secondaryDark, surfaceDarkest } = useTheme();
+  const { surfaceLighter, secondaryDark, surfaceDarkest, textDark } =
+    useTheme();
 
   return (
     <Card
-      onClick={onCheck}
+      onClick={disabled ? undefined : onCheck}
       borderRadius="md"
       sx={{
         ...sx,
         display: "flex",
         alignItems: "center",
         boxSizing: "border-box",
-        cursor: "pointer",
+        cursor: disabled ? undefined : "pointer",
         padding: checked ? "6px 15px 6px 6px" : "7px 16px 7px 7px",
         borderColor: checked ? secondaryDark : undefined,
         borderWidth: checked ? "2px" : undefined,
         ":hover": {
-          borderColor: checked ? undefined : surfaceDarkest,
+          borderColor: checked || disabled ? undefined : surfaceDarkest,
         },
+        backgroundColor: disabled ? surfaceLighter : undefined,
       }}
       // @ts-ignore mui typing issue, component prop is not being recognized
       component="button"
@@ -49,8 +53,13 @@ const RadioCard: React.FC<RadioButtonCardProps> = ({
           },
         }}
         checked={checked}
+        disabled={disabled}
       />
-      {text && <Text variant="bodyL">{text}</Text>}
+      {text && (
+        <Text variant="bodyL" color={disabled ? textDark : undefined}>
+          {text}
+        </Text>
+      )}
       {children}
     </Card>
   );
