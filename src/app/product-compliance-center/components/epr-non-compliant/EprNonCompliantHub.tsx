7ee85b15aadf222ab7dc3b7d5dfb1c304897bf33
Zhuyun Dai <zdai@wish.com>
2023-06-21 16:27:00 -0400

    [MKL-76050] add notice statements on the top of epr non-compliant page (#284)
    * [MKL-76050] add notice statements on the top of epr non-compliant page
    
    https://jira.wish.site/browse/MKL-76050
    
    * [MKL-76050] use Alert instead

--- src/app/product-compliance-center/components/epr-non-compliant/EprNonCompliantHub.tsx
+++ src/app/product-compliance-center/components/epr-non-compliant/EprNonCompliantHub.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { Card } from "@ContextLogic/atlas-ui";
+import { Card, Alert, Text } from "@ContextLogic/atlas-ui";
 import EprNonCompliantSummaryArea from "./EprNonCompliantSummaryArea";
 import EprNonCompliantProductsArea from "./EprNonCompliantProductsArea";
 
@@ -12,6 +12,23 @@ const EprNonCompliantHub: React.FC = () => {
         padding: "24px",
       }}
     >
+      <Alert
+        severity="warning"
+        sx={{
+          marginBottom: "24px",
+        }}
+      >
+        <Text>
+          Due to an upcoming product categorization update, there may be some
+          changes to the Extended Producer Responsibility (EPR) categories for
+          your products shipping to EPR regions starting August 27, 2023, UTC.
+        </Text>
+        <Text>
+          To comply, you must submit EPR registration numbers for these
+          categories to Wish before this date or risk products in these
+          categories being blocked for sale in applicable EPR countries.
+        </Text>
+      </Alert>
       <EprNonCompliantSummaryArea />
       <EprNonCompliantProductsArea />
     </Card>
