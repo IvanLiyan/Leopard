d0653589f3d6cc77e18b36fc0e8eb4661ada4cf1
pmanoharan-wish <134536612+pmanoharan-wish@users.noreply.github.com>
2023-08-02 13:30:26 +0530

    Added translation (#341) (#343)

--- src/app/promotions/eligible-products-csv/components/SubmitRequestInfoCard.tsx
+++ src/app/promotions/eligible-products-csv/components/SubmitRequestInfoCard.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { observer } from "mobx-react";
 import { Card, CardProps } from "@ContextLogic/atlas-ui";
 import Markdown from "@core/components/Markdown";
+import { i18n } from "@core/toolkit/i18n";
 
 const markdown = `
-- Merchants will receive an email with the CSV files within 24 hours of submission.
+- ${i18n(
+  "Merchants will receive an email with the CSV files within 24 hours of submission.",
+)}
 
-- Only one request per promotion type/event ID can be submitted at one time.
+- ${i18n(
+  "Only one request per promotion type/event ID can be submitted at one time.",
+)}
 `;
 
 const SubmitRequestCard: React.FC<Omit<CardProps, "children">> = ({
